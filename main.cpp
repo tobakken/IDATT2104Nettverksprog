f6c3d919a5cb6f5ae0c50466c6bc04cffd837d15
tobakken <tobakken@stud.ntnu.no>
2022-01-12 19:42:41 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+
+class CheckPrime {
+    public:
+        static bool checkPrime(int nr) {
+            if (nr < 2) return false;
+            for (int i = 2; i < nr; ++i) {
+                if (nr % i == 0) return false;
+            }
+            return true;
+        }
+    };
+
+int main() {
+    vector<thread> threads;
+    vector<int> primes;
+    int start = 0;
+    int stop = 10;
+    int nrOfThreads = 3;
+
+    threads.reserve(nrOfThreads);
+for (int i = 0; i < nrOfThreads; ++i) {
+        threads.emplace_back([&start, &stop, &primes] {
+            int j = start;
+            while (j < stop) {
+                if (CheckPrime::checkPrime(j)) primes.push_back(j);
+                ++j;
+            }
+        });
+    }
+
+
+    for (auto const &value: primes) cout << value << endl;
+    for (auto &thread: threads) thread.join();
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
