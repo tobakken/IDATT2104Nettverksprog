205b4f4c9d9919e4eb2dee12a82cab80838f6fc0
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2022-02-01 15:50:02 +0100

    Asnmt 2 finished

--- workers.cpp
+++ workers.cpp
@@ -5,22 +5,22 @@
 #include <list>
 #include <condition_variable>
 #include <mutex>
+#include <chrono>
 
 using namespace std;
+using namespace chrono;
 
-int main() {
-    cout << "Hello" << endl;
-}
 
 class Workers {
-public:
+private:
     int nrOfThreads;
     vector<thread> worker_threads;
     list<function<void()>> tasks;
     mutex task_mutex;
     condition_variable cv;
     bool thread_run;
-
+    bool unfinished_tasks;
+public:
     Workers(int nr) {
         nrOfThreads = nr;
     }
@@ -33,23 +33,81 @@ public:
                     function<void()> task;
                     {
                         unique_lock<mutex> lock(task_mutex);
+                        while(!unfinished_tasks){
+                            cv.wait(lock);
+                        }
                         if (!tasks.empty()) {
                             task = *tasks.begin();
                             tasks.pop_front();
                         }
                     }
-                    task();
+                    if (task) task();
                 }
             });
         }
     }
 
     void post(function<void()> func) {
+        unique_lock<mutex> lock(task_mutex);
         tasks.emplace_back(func);
+        unfinished_tasks = true;
+        cv.notify_one();
     }
 
-    void join() {
-        if (tasks.empty()) thread_run = false;
+    void post_timeout(function<void()> func, int s){
+        this_thread::sleep_for(milliseconds (s));
+        unique_lock<mutex> lock(task_mutex);
+        tasks.emplace_back(func);
+        unfinished_tasks = true;
+        cv.notify_one();
+    }
+
+    void stop() {
+        thread_run = false;
+        cv.notify_all();
         for (auto &thread: worker_threads) thread.join();
     }
 };
+
+int main() {
+    Workers worker_threads(5);
+    Workers event_loop(1);
+
+    worker_threads.start();
+    event_loop.start();
+
+    for (int i = 0; i < 7; ++i) {
+        worker_threads.post([i] {
+            for (int j = 0; j < 1000000; ++j) {
+                for (int k = 0; k < 1000; ) {
+                    k++;
+                }
+            }
+            cout << "worker_threads task: " << i << ", running in thread: " << this_thread::get_id() << endl;
+        });
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        worker_threads.post([i] {
+            for (int j = 0; j < 1000000; ++j) {
+                for (int k = 0; k < 1000; ) {
+                    k++;
+                }
+            }
+            cout << "event_loop task: " << i << ", running in thread: " << this_thread::get_id() << endl;
+        });
+    }
+
+    worker_threads.post_timeout([] {
+        cout << "timed task, running in thread:" << this_thread::get_id() << endl;
+    }, 5000);
+
+    worker_threads.post([] {
+        cout << "different task, running in thread:" << this_thread::get_id() << endl;
+    });
+
+    this_thread::sleep_for(3s);
+    cout<<"wait finished"<<endl;
+    worker_threads.stop();
+    event_loop.stop();
+}
