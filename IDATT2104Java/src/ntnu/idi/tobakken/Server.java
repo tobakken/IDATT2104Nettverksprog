f8091dbead0810df0f174f741579941f2fc51a27
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2022-02-10 14:45:38 +0100

    Complete asnm P3

--- IDATT2104Java/src/ntnu/idi/tobakken/Server.java
+++ IDATT2104Java/src/ntnu/idi/tobakken/Server.java
@@ -5,62 +5,96 @@ import java.net.*;
 import java.util.Locale;
 import java.util.Objects;
 
-public class Server {
+public class Server{
+
     public static void main(String[] args) throws IOException {
         final int PORTNR = 1250;
 
         ServerSocket tjener = new ServerSocket(PORTNR);
-        System.out.println("Log for the server. Now we wait...");
-        Socket forbindelse = tjener.accept();  // venter inntil noen tar kontakt
 
-        /* Åpner strømmer for kommunikasjon med klientprogrammet */
-        InputStreamReader leseforbindelse
-                = new InputStreamReader(forbindelse.getInputStream());
-        BufferedReader leseren = new BufferedReader(leseforbindelse);
-        PrintWriter skriveren = new PrintWriter(forbindelse.getOutputStream(), true);
+        while (true) {
+            Socket connection = null;
+            try {
+                System.out.println("Log for the server. Now we wait...");
+                connection = tjener.accept();  // venter inntil noen tar kontakt
+                /* Åpner strømmer for kommunikasjon med klientprogrammet */
+                InputStreamReader leseforbindelse
+                        = new InputStreamReader(connection.getInputStream());
+                BufferedReader leseren = new BufferedReader(leseforbindelse);
+                PrintWriter skriveren = new PrintWriter(connection.getOutputStream(), true);
+                Thread t = new ClientHandler(connection, leseren, skriveren);
+                t.start();
+            }
+            catch (Exception e) {
+                connection.close();
+                e.printStackTrace();
+            }
 
-        /* Sender innledning til klienten */
-        skriveren.println("Hello, you have contact with the server!");
+        }
+    }
+
+
+}
+
+class ClientHandler extends Thread {
+    Socket connection;
+    final BufferedReader reader;
+    final PrintWriter writer;
+
+    public ClientHandler(Socket connection, BufferedReader reader, PrintWriter writer) {
+        this.connection = connection;
+        this.reader = reader;
+        this.writer = writer;
+    }
+
+    @Override
+    public void run(){
+        super.run();
+        writer.println("Hello, you have contact with the server!");
         String enLinje = "Lorem";
         String valgLinje = "Ipsum";
         int x = 0;
         int y = 0;
 
         while (enLinje != null) {  // forbindelsen på klientsiden er lukket
-            skriveren.println("Type your first number:");
-            enLinje = leseren.readLine();  // mottar en linje med tekst
-            x = Integer.parseInt(enLinje);
+            try {
+                writer.println("Type your first number:");
+                enLinje = reader.readLine();  // mottar en linje med tekst
+                x = Integer.parseInt(enLinje);
 
-            skriveren.println("Type your second number:");
-            enLinje = leseren.readLine();
-            y = Integer.parseInt(enLinje);
+                writer.println("Type your second number:");
+                enLinje = reader.readLine();
+                y = Integer.parseInt(enLinje);
 
-            skriveren.println("What do you want (choose 1 or 2 and press enter)" +
-                    " (1) Addition" +
-                    "  (2) Subtraction");
-            valgLinje = leseren.readLine();
+                writer.println("What do you want (choose 1 or 2 and press enter)" +
+                        " (1) Addition" +
+                        "  (2) Subtraction");
+                valgLinje = reader.readLine();
 
-            if (valgLinje.equals("1")){
-                skriveren.println("Result of addition: " + add(x, y) );  // sender svar til klienten
-            } else if (valgLinje.equals("2")){
-                skriveren.println("result of subtraction: " + subtract(x, y) );
-            } else {
-                skriveren.println("Something went wrong, feel free to try again");
+                if (valgLinje.equals("1")){
+                    writer.println("Result of addition: " + add(x, y) );  // sender svar til klienten
+                } else if (valgLinje.equals("2")){
+                    writer.println("result of subtraction: " + subtract(x, y) );
+                } else {
+                    writer.println("Something went wrong, feel free to try again");
+                }
+
+                writer.println("Type 'yes' to go again");
+
+                enLinje = reader.readLine();
+                System.out.println(enLinje);
+                if (!enLinje.equals("yes")) enLinje = null;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-
-            skriveren.println("Type 'yes' to go again");
-
-            enLinje = leseren.readLine();
-            System.out.println(enLinje);
-            if (!enLinje.equals("yes")) enLinje = null;
         }
-
-        /* Lukker forbindelsen */
-        leseren.close();
-        skriveren.close();
-        forbindelse.close();
+        try {
+            this.writer.close();
+            this.reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
     public static int add(int x, int y){
         return x + y;
     }
@@ -69,4 +103,3 @@ public class Server {
         return x - y;
     }
 }
-
---
+++ IDATT2104Java/src/ntnu/idi/tobakken/WebServer.java
@@ -0,0 +1,34 @@
+package ntnu.idi.tobakken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+    public static void main(String[] args) throws IOException {
+        final int PORTNR = 80;
+
+        ServerSocket server = new ServerSocket(PORTNR);
+        Socket connection = server.accept();
+
+        PrintWriter pw = new PrintWriter(connection.getOutputStream(), true);
+
+        String msg = "HTTP/1.0 200 OK \n" +
+                "Content-Type: text/html; charset=utf-8 \n" +
+                "\n\n" +
+                "<html> <body>" +
+                "<h1> Hilsen. Du har koblet deg opp til min enkle web-tjener</h1>" +
+                "<p>Header fra Klient er: </p>" +
+                "<ul>" +
+                "<li>HTTP/1.0 200 OK </li>" +
+                "<li>Content-Type: text/html; charset=utf-8</li>" +
+                "</ul>" +
+                "</body></html>";
+        pw.println(msg);
+        connection.close();
+        pw.close();
+    }
+}
