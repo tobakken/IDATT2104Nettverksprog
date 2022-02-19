51e6c15fa42361e3321f482058a91b8484d23c61
tobakken <tobakken@stud.ntnu.no>
2022-02-19 16:18:11 +0100

    Fixed asmnt 3 to read header from klient.

--- IDATT2104Java/src/ntnu/idi/tobakken/WebServer.java
+++ IDATT2104Java/src/ntnu/idi/tobakken/WebServer.java
@@ -14,9 +14,16 @@ public class WebServer {
         ServerSocket server = new ServerSocket(PORTNR);
         Socket connection = server.accept();
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         PrintWriter pw = new PrintWriter(connection.getOutputStream(), true);
 
-        //Skulle hatt med loop som lagret headeren fra browseren og vist den som html
+        StringBuilder head = new StringBuilder("<ul>");
+        for (int i = 0; i < 16; i++) {
+            head.append("<li>").append(reader.readLine()).append("</li>");
+        }
+        head.append("</ul>");
+
+        System.out.println(head);
 
         String msg = "HTTP/1.0 200 OK \n" +
                 "Content-Type: text/html; charset=utf-8 \n" +
@@ -24,10 +31,7 @@ public class WebServer {
                 "<html> <body>" +
                 "<h1> Yo dawg! Du har koblet deg opp til min enkle web-tjener</h1>" +
                 "<p>Header fra Klient er: </p>" +
-                "<ul>" +
-                "<li>HTTP/1.0 200 OK </li>" +
-                "<li>Content-Type: text/html; charset=utf-8</li>" +
-                "</ul>" +
+                head +
                 "</body></html>";
         pw.println(msg);
         connection.close();
