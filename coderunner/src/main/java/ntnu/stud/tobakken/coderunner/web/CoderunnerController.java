c931580a36f2c342b5d500039bca69544e4fe806
Tor Oyvind Bakken <tobakken@stud.ntnu.no>
2022-03-05 16:56:29 +0100

    Ready to give up on compiling and running code in docker in one command

--- coderunner/src/main/java/ntnu/stud/tobakken/coderunner/web/CoderunnerController.java
+++ coderunner/src/main/java/ntnu/stud/tobakken/coderunner/web/CoderunnerController.java
@@ -16,34 +16,47 @@ public class CoderunnerController {
     public String run(@RequestBody String code) throws IOException {
         FileService fileHandler = new FileService();
         File codeFile = new File("src/main/java/ntnu/stud/tobakken/coderunner/docker/cppCode.cpp");
-        fileHandler.writeToFile(codeFile, code);
+        String codetrim = code.replaceAll("\"", "");
+        fileHandler.writeToFile(codeFile, codetrim);
 
-        String command = "docker run gcc:latest echo '" + code +
-                "' >> cppCode.cpp" +
-                " && gcc cppCode.cpp -lstdc++ && ./a.out";
+        String command = "docker run -i --rm gcc bin/bash -c \"echo \'" + codetrim + "\' >> hello.cpp && cat hello.cpp \"";
+        File commandFile = new File("src/main/java/ntnu/stud/tobakken/coderunner/docker/command.bat");
+        fileHandler.writeToFile(commandFile, command);
 
-        Process p = Runtime.getRuntime().exec(command);
+
+        Process proc = Runtime.getRuntime().exec("docker run -i --rm gcc bin/bash -c \"echo \'" + codetrim + "\' >> hello.cpp && cat hello.cpp \" ");
 
         BufferedReader stdInput = new BufferedReader(new
-                InputStreamReader(p.getInputStream()));
+                InputStreamReader(proc.getInputStream()));
 
         BufferedReader stdError = new BufferedReader(new
-                InputStreamReader(p.getErrorStream()));
+                InputStreamReader(proc.getErrorStream()));
+
+        StringBuilder output = new StringBuilder();
+        StringBuilder outputError = new StringBuilder();
 
 // Read the output from the command
         System.out.println("Here is the standard output of the command:\n");
-        String s = null;
+        String s = stdInput.readLine();
+        System.out.println(s);
         while ((s = stdInput.readLine()) != null) {
             System.out.println(s);
+            output.append(s).append("\n");
         }
 
 // Read any errors from the attempted command
         System.out.println("Here is the standard error of the command (if any):\n");
         while ((s = stdError.readLine()) != null) {
             System.out.println(s);
+            outputError.append(s);
         }
 
-        return "Hey";
+        String outputString = output.toString();
+        String outputErrorString = outputError.toString();
+
+        System.out.println(outputString);
+        if (outputErrorString.length() != 0) return outputErrorString;
+        return outputString;
     }
 
     @GetMapping("/hello")
